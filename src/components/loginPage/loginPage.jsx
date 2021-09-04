e309c21c957f9f08fa647f7aa6648f4899d3f450
sungmin-choi <namja306@naver.com>
2021-09-04 14:28:21 +0900

    test

--- src/components/loginPage/loginPage.jsx
+++ src/components/loginPage/loginPage.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import styles from './loginPage.module.css';
 const LoginPage = (props) =>{
     return(
-        <h1>login Page!</h1>
+        <div className={styles.main}>
+            <div className={styles.loginform}>
+                <header className={styles.header}>
+                    <img className={styles.logo} src='/favicon.png' alt="logo" />
+                    <p className={styles.title1}>Business Card Maker</p>
+                </header>
+                <section className={styles.buttonForm}>
+                <p className={styles.title2}>Login</p>
+                <button className={styles.googleLogin}>Google</button>
+                <button className={styles.githubLogin}>Github</button>
+                </section>
+                <footer className={styles.footer}>Code your dream</footer>
+            </div>
+
+        </div>
     );
 }
 
